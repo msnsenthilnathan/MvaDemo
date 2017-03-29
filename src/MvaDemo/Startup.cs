3e975399c34c5f98fe41d50893ac456cd8a784c0
msnsenthilnathan <msnsenthilnathan@hotmail.com>
2017-03-29 20:15:57 +0530

    removed logger and other stuff

--- src/MvaDemo/Startup.cs
+++ src/MvaDemo/Startup.cs
@@ -19,14 +19,14 @@ namespace MvaDemo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
-            loggerFactory.AddConsole();
+            //loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
             app.Run(async (context) =>
             {
